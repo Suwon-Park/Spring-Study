2c0b07bffcdacf00fb4cb1cc640816b10dfdd529
Suwon <116046530+Suwon-Park@users.noreply.github.com>
2023-10-16 21:56:09 +0900

    Delete test/assignment directory

--- test/assignment/assignment/src/main/java/com/example/assignment/repository/MemberRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.assignment.repository;
-
-import com.example.assignment.domain.MemberEntity;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface MemberRepository extends JpaRepository<MemberEntity, Long> {
-
-
-}
